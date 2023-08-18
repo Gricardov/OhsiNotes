6c985454958bc9e69edc6c2de40eeb002d4e0dea
gricardov <gricardov@gmail.com>
2023-08-18 13:19:46 -0500

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,94 +1,65 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client"; // This is a client component 👈🏽
+
+import { useState } from 'react';
+import styles from './page.module.css';
 
 export default function Home() {
+
+  const [text, setText] = useState('');
+  const [querying, setQuerying] = useState(false);
+  const [completion, setCompletion] = useState('');
+
+  const query = async (type = 1) => {
+    if (querying) return;
+    try {
+      setQuerying(true);
+      const data = await fetch('/api/chat', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          messages: [{
+            role: "user",
+            content: text.trim()
+          }],
+          type
+        })
+      });
+      const { completion } = await data.json();
+      setCompletion(completion);
+      setQuerying(false);
+    } catch (error) {
+      setQuerying(false);
+      console.log(error);
+      alert('Ocurrió un error');
+    }
+  }
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className={styles.container}>
+        <div className={styles.container__left}>
+          <h3 className={styles.title}></h3>
+          <textarea
+            value={text}
+            onChange={e => setText(e.target.value)}
+            className={styles.container__textarea} />
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className={styles.container__right}>
+          <div className={styles.container__header}>
+            <div className={styles.buttongroup}>
+              <button disabled={querying} onClick={() => query(1)}>Crear clase</button>
+              <button disabled={querying} onClick={() => query(2)}>Hacer preguntas</button>
+              <button disabled={querying} onClick={() => query(3)}>Test</button>
+            </div>
+          </div>
+          <h3 className={styles.title}></h3>
+          <textarea
+            value={querying ? 'Analizando...' : completion}
+            disabled
+            className={styles.container__textarea} />
+        </div>
       </div>
     </main>
   )
---
+++ src/pages/api/chat.js
@@ -0,0 +1,50 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+    organization: 'org-CdlYjzEbK0kG75DSByz95dNk',
+    apiKey: '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'
+});
+
+export default async function handler(req, res) {
+    if (req.method === 'POST') {
+
+        const openai = new OpenAIApi(configuration);
+
+        try {
+            const { messages, type } = req.body;
+
+            let instruction = '';
+
+            switch (type) {
+                case 1:
+                    instruction = 'Crea una clase divertida a partir de mis apuntes de clase. Que sea en máximo 100 palabras';
+                    break;
+                case 2:
+                    instruction = 'Hazme preguntas reflexivas sobre mis apuntes de clase y respondelas. Que sea en máximo 200 palabras';
+                    break;
+                case 3:
+                    instruction = 'Dime algo interesante sobre el siguiente texto en solo tres palabras';
+                    break;
+                default:
+                    break;
+            }
+
+            console.log('messages', messages);
+            const completion = await openai.createChatCompletion({
+                model: 'gpt-3.5-turbo',
+                messages: [{ role: 'system', content: instruction },
+                ...messages
+                ]
+            });
+            res.json({
+                completion: completion.data.choices[0].message?.content
+            });
+            //res.status(200).json({ message: 'Oki' });
+        } catch (error) {
+            console.log('Error: ', error.message);
+            res.status(500).json({ message: 'Error' });
+        }
+    } else {
+        res.status(500).json({ message: 'Unauthorized' });
+    }
+}
